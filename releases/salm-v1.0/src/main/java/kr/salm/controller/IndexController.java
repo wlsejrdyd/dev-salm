6a8ab03fb87e74e526dad189179342087003c09a
wlsejrdyd <wlsejrdyd@gmail.com>
2025-04-25 21:55:41 +0900

    auto deploy test 1

---
+++ releases/salm-v1.0/src/main/java/kr/salm/controller/IndexController.java
@@ -0,0 +1,33 @@
+package kr.salm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    @GetMapping("/questions")
+    public String questionList() {
+        return "question";
+    }
+
+    @GetMapping("/ask")
+    public String askForm() {
+        return "ask";
+    }
+}
