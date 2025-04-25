e401a0314da96c7340f5c319a9a1f5b7edc1efc6
찐떡 <130738855+wlsejrdyd@users.noreply.github.com>
2025-04-26 00:39:59 +0900

    .
    ..?

---
+++ releases/salm-v1.1/src/main/java/kr/salm/SalmApplication.java
@@ -0,0 +1,11 @@
+package kr.salm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SalmApplication.class, args);
+    }
+}
